464d86095fe69eb902bfc6ba48423ec1aa668be5
John Ramirez <jero_49@outlook.com>
2023-04-09 19:01:14 -0500

    Prueba pasada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ import { getColor } from "./color-service";
 
 export function App() {
   const [counter, setCounter] = useState(0);
-  const [color, setColor] = useState("red");
+  const [color, setColor] = useState("blue");
+
+  const handleIncrease = () => {
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+    setColor("blue");
+  };
+
+  const handleDecrease = () => {
+    const newCounter = counter - 1;
+    setCounter(newCounter);
+    setColor("red");
+  };
+
+  const handleReset = () => {
+    setCounter(0);
+    setColor("red");
+  };
 
   return (
     <div>
@@ -14,9 +31,9 @@ export function App() {
         <span className={color}>Counter:{counter}</span>
       </div>
 
-      <button>Increase</button>
-      <button>Decrease</button>
-      <button>Reset</button>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }

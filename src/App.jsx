ce9da2d348a8d5a749ed532e2463a2f82d15ba9d
John Ramirez <elcuartoespadaulquiorra@gmail.com>
2023-06-05 11:42:59 -0500

    Router 1 aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Menu } from "./components/Menu";
+import { HomePage } from "./pages/HomePage";
+import { EventListPage } from "./pages/EventListPage"
+import { EventDetailPage } from './pages/EventDetailPage'
+import { TicketPaymentPage } from "./pages/TicketPaymentPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export function AppRouter() {
   return (
     <>
       <Menu />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<EventListPage />} />
+        <Route path="/events/:eventId" element={<EventDetailPage />} />
+        <Route path="/events/:eventId/tickets/:priceId" element={<TicketPaymentPage />} />
+      </Routes>
     </>
   );
 }
 
 export function App() {
   return (
-    <div>
+    <BrowserRouter>
       <AppRouter />
-    </div>
+    </BrowserRouter>
   );
 }
